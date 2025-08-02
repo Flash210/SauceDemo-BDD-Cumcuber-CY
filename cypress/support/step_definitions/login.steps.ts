b52a72da36e42bb73de56fcdb8311a794f24ff98
houssem <houcem.eddine.mosbah@gmail.com>
2025-08-02 22:01:09 +0200

    Remove ignored files from repo

--- cypress/support/step_definitions/login.steps.ts
+++ cypress/support/step_definitions/login.steps.ts
@@ -1,48 +1,4 @@
-// import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
-// import { LoginSelectors } from '../../e2e/selectors/login/login_selectors';
 
-// Given('I am on the login page', () => {
-//  // cy.visit('/login');
-//  cy.visit('https://www.saucedemo.com'); // Ensure the base URL is set correctly in cypress.config.ts
-// });
-
-// When('I enter valid credentials', () => {
-//   cy.get(LoginSelectors.usernameField).type('standard_user');
-//   cy.get(LoginSelectors.passwordField).type('secret_sauce');
-//   cy.get(LoginSelectors.loginButton).click();
-// });
-
-// Then('I should see the inventory page', () => {
-//   cy.url().should('include', '/inventory');
-// });
-
-// // Empty username
-// When('I login with an empty username and a valid password', () => {
-//   cy.get(LoginSelectors.usernameField).clear();
-//   cy.get(LoginSelectors.passwordField).type('secret_sauce');
-//   cy.get(LoginSelectors.loginButton).click();
-// });
-
-// // Empty password
-// When('I login with a valid username and an empty password', () => {
-//   cy.get(LoginSelectors.usernameField).type('standard_user');
-//   cy.get(LoginSelectors.passwordField).clear();
-//   cy.get(LoginSelectors.loginButton).click();
-// });
-
-// // Invalid credentials
-// When('I login with invalid credentials', () => {
-//   cy.get(LoginSelectors.usernameField).type('wrong_user');
-//   cy.get(LoginSelectors.passwordField).type('wrong_pass');
-//   cy.get(LoginSelectors.loginButton).click();
-// });
-
-// // Error message assertion
-// Then('I should see an error message {string}', (errorMessage: string) => {
-//   cy.get(LoginSelectors.errorMessage)
-//     .should('be.visible')
-//     .and('contain.text', errorMessage);
-// });
 import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
 Given('I am on the login page', () => {
